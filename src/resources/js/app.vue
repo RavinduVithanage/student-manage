a18b82b9646b7cba6e200060001a8b7172f42960
Ravindu Vithanage <vithanage@thesanmark.com>
2024-03-30 21:50:12 +0530

    login form

--- src/resources/js/app.vue
+++ src/resources/js/app.vue
@@ -1,5 +1,13 @@
+<script setup>
+
+import login from './components/login-form.vue'
+
+
+</script>
 <template>
+
     <h1>
         How To Install Vue 3 in Laravel 10 : Laravel SPA :)
     </h1>
+    <login/>
 </template>
